34f21ea5c01e8b2e7b876f0cfd061610a2108d18
Admin <Admin@SALMAAMRO>
2025-08-17 16:57:48 +0300

    Q1

--- Ass2AdvancedC#/Program.cs
+++ Ass2AdvancedC#/Program.cs
@@ -1,9 +1,48 @@
-﻿namespace Ass2AdvancedC_
+﻿using System.Collections;
+
+namespace Ass2AdvancedC_
 {
     internal class Program
     {
+        #region Q1 
+        //public static void ReverseArrayList(ArrayList ArrayList)
+        //{
+        //    int FirstIndex = 0;
+        //    int LastIndex = ArrayList.Count - 1;
+
+        //    while (FirstIndex < LastIndex)
+        //    { 
+        //        object? Temp = ArrayList[FirstIndex];
+        //        ArrayList[FirstIndex] = ArrayList[LastIndex];
+        //        ArrayList[LastIndex] = Temp;
+
+        //        FirstIndex++;
+        //        LastIndex--;
+
+        //    }
+
+        //}
+        #endregion
         static void Main(string[] args)
         {
+            #region CallQ1
+            //ArrayList arrayList = new ArrayList();
+            //arrayList.AddRange(new int[] { 1, 2, 3, 4 });
+            //Console.WriteLine("Original ArrayList:");
+            //foreach (int item in arrayList)
+            //{
+            //    Console.Write(item + " ");
+            //}
+            //Console.WriteLine();
+            //ReverseArrayList(arrayList);
+            //Console.WriteLine("Original ArrayList:");
+            //foreach (int item in arrayList)
+            //{
+            //    Console.Write(item + " ");
+            //}
+
+            #endregion
+
 
         }
     }
